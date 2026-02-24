698b3a4b227243a61ba8f08ceae2adcd969a6804
Sandesh Pandey <sandesh143pan@gmail.com>
2026-02-24 18:48:36 +0530

    Add files via upload

---
+++ xyz.js
@@ -0,0 +1,23 @@
+var a = 10;
+var a = 20;
+{
+    var b = 30;
+    console.log(typeof b);
+}
+
+
+var myName = " ram";
+console.log(myName);
+
+var myName = "shyam";
+console.log(myName)
+
+let hero = "shaktimaan";
+console.log(hero)
+ hero = "sitata"
+console.log(hero)
+
+
+
+
+
