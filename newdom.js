c65f37fe7c7296c102507b41b23664afed4c7b6b
Sandesh Pandey <sandesh143pan@gmail.com>
2026-03-17 15:59:13 +0530

    Add files via upload

---
+++ newdom.js
@@ -0,0 +1,9 @@
+function Result(){
+
+     let changeColor=document.getElementById('text');
+ changeColor.style.color="red";
+
+ let bodyColor=document.getElementsByTagName('body')[0];
+ bodyColor.style.backgroundColor="skyblue"; 
+ 
+}
