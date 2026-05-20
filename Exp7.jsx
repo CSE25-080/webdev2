e38dc07a8ebf2336adee58952e62f81a8b34b911
Sandesh Pandey <sandesh143pan@gmail.com>
2026-05-21 02:52:35 +0530

    Add files via upload

---
+++ Exp7.jsx
@@ -0,0 +1,21 @@
+
+
+function Exp7() {
+  return (
+   <>
+   <h1>Experiment 07</h1>
+   <div style={{backgroundColor:"#d23b3b", display:'flex', gap:'10px'}}>
+   <div>
+     <h2>{name}</h2>
+   </div>
+<p>
+    welcome😊,!! In my js world....
+    
+</p>
+   </div>
+
+   </>
+  )
+}
+
+export default Exp7
