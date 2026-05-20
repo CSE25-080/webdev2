8eb03b0d900ceaabcec0950b7f8a31f3f70577b8
Sandesh Pandey <sandesh143pan@gmail.com>
2026-05-21 02:58:28 +0530

    Add files via upload

---
+++ Props.jsx
@@ -0,0 +1,14 @@
+
+
+function Props(Props) {
+  return (
+    <>
+    <h1>Props</h1>
+    <h2>{Props.name}</h2>
+    <h2>{Props.rollno}</h2>
+    <h2>{Props.course}</h2>
+    </>
+  )
+}
+
+export default Props
