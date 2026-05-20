4cda0a4032628728ed25d7954ad80114dcef34e1
Sandesh Pandey <sandesh143pan@gmail.com>
2026-05-21 02:58:11 +0530

    Add files via upload

---
+++ Demo.jsx
@@ -0,0 +1,11 @@
+function Demo(){
+    return(
+        <>
+        <h1>Demo Component </h1>
+        <p>
+            know about demo component 
+        </p>
+        </>
+    );
+}
+export default Demo;
